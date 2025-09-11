2fd10a7aa0c859d1e65c2e40ac2965a913b6a725
Grigory Smolyak <smol.grisha2007@gmail.com>
2025-09-11 17:41:35 +0300

    test project

---
+++ notes/tests/test_routes.py
@@ -0,0 +1,52 @@
+# type: ignore
+from http import HTTPStatus
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse 
+from notes.models import Note
+
+
+User = get_user_model()
+
+
+class TestRoutes(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.author = User.objects.create(username='Автор')
+        cls.reader = User.objects.create(username='Посетитель')
+        cls.note = Note.objects.create(
+            title='Заголовок',
+            text='Текст',
+            slug='address-for-page-with-note',
+            author=cls.author
+        )
+
+    def test_pages_availability(self):
+        urls = (
+            ('notes:home', None),
+            ('users:login', None),
+            ('users:signup', None)
+        )
+        for name, args in urls:
+            with self.subTest(name=name):
+                url = reverse(name, args=args)
+                response = self.client.get(url)
+                self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_availability_for_note_edit_and_delete(self):
+        users_statuses = (
+            (self.author, HTTPStatus.OK),
+            (self.reader, HTTPStatus.NOT_FOUND),
+        )
+
+        for user, status in users_statuses:
+            self.client.force_login(user)
+            for name in ('notes:edit', 'notes:delete'):
+                with self.subTest(user=user, name=name):
+                    url = reverse(name, args=(self.note.slug,))
+                    response = self.client.get(url)
+                    self.assertEqual(response.status_code, status)
+    
+
+    
